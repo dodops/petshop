8560c23943d11e3ea0046c7001da780002077774
Douglas Macedo <dodop3000@gmail.com>
2017-03-17 14:02:24 -0300

    dog factories

---
+++ spec/factories/dogs.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :dog do
+    castrated true
+    gender "female"
+    birth_date "2010-03-17"
+    name Faker::Cat.name
+    last_visit "2017-03-17"
+
+    association :race, factory: :race
+    association :owner, factory: :owner
+  end
+
+  factory :invalid_dog, parent: :dog do
+    castrated nil
+    gender nil
+    birth_date nil
+    name nil
+    last_visit nil
+  end
+end
