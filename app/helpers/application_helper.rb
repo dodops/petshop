a65c690d501a9a5f210d1b11df6bc291b4a36e51
Douglas Macedo <dodop3000@gmail.com>
2017-03-18 11:43:34 -0300

    translation helper and .yml files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def translate_attribute(model, attribute, count = 1)
+    model.human_attribute_name attribute, count: count
+  end
+
+  alias ta translate_attribute
 end
